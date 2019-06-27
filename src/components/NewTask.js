e4cdc3e375809fb8a5ef4a804c748276f9f3e0e2
James <james@experimentality.co>
2019-06-27 15:15:31 -0500

    Configure newTask

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 
-function NewTask() {
-    return (
-        <div className="app__new-task">
-            <div className="app__new-task--form-container">
-                <input type="text" placeholder="Título..."/>
-                <textarea placeholder="Contenido..."/>
-                <button>Crear</button>
+const POST_MUTATION = gql`
+    mutation post( $title: String!, $description: String!) {
+        post(title: $title, description: $description) {
+            id
+            title
+            description
+        }
+    }
+`;
+
+class NewTask extends Component {
+    state = {
+        title: '',
+        description: '',
+    }
+
+    render(){
+        const { title, description } = this.state
+        return (
+            <div className="app__new-task">
+                <div className="app__new-task--form-container">
+                    <input
+                        type="text"
+                        placeholder="Título..."
+                        value={title}
+                        onChange={e => this.setState({ title: e.target.value })}/>
+                    <textarea placeholder="Contenido..."
+                              value={description}
+                              onChange={e => this.setState({ description: e.target.value })}/>
+
+                    <Mutation mutation={POST_MUTATION} variables={{ title, description }}>
+                        {() => (
+                            <Mutation mutation={POST_MUTATION} variables={{ title, description }}>
+                                {postMutation => <button onClick={postMutation}>Submit</button>}
+                            </Mutation>
+                        )}
+                    </Mutation>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
 export default NewTask;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,31 +3,6 @@ import TaskItem from './TaskItem'
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
-// const tasks = [
-//     {
-//         id: '654B6A6576E5C7865',
-//         title: 'Deploy production application',
-//         description: 'Install production application through Jenkins',
-//         createdAt: new Date().toJSON(),
-//         checked: false,
-//         category: {
-//             id: '094B6A1176E5C7887' ,
-//             name: 'Important'
-//         }
-//     },
-//     {
-//         id: '23234234234HH3H33',
-//         title: 'Go to the doctor',
-//         description: 'Call the office to confirm availability',
-//         createdAt: new Date().toJSON(),
-//         checked: false,
-//         category: {
-//             id: '094B6A1176E5C7887' ,
-//             name: 'Important'
-//         }
-//     }
-// ]
-
 const FEED_QUERY = gql`
     {
         tasks {
